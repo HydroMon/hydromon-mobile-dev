0c67e0c5af32b2b5565397b9a3660804c3a68d43
akmal09 <akmal.119140203@student.itera.ac.id>
2022-06-06 20:16:33 +0600

    ChooseRole and SignUp initiate

---
+++ app/src/main/java/com/example/hydromon/SignUpHydroponicsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hydromon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpHydroponicsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_hydroponics)
+    }
+}
