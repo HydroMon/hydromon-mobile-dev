fec0beae1e5eb1fa461c3c9f664730f309d42ba8
puntario <riopuntakusuma@gmail.com>
2022-06-08 13:44:16 +0700

    1

---
+++ app/src/main/java/com/example/hydromon/ui/authentication/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hydromon.ui.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hydromon.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
