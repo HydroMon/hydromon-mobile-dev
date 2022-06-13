f5e3c8e1a12938ad4efedc59e25d3e657ff357df
puntario <riopuntakusuma@gmail.com>
2022-06-13 21:40:34 +0700

    2

---
+++ app/src/main/java/com/example/hydromon/EnterCodeViewerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hydromon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EnterCodeViewerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_code_viewer)
+    }
+}
